6af7bedec054316ec8892cfb5e53415d57e99444
Stefan Ring <stefanrin@gmail.com>
2026-01-31 14:49:39 +0100

    this should do it for now (dummy audio stream)

--- src/gui/render/render.cpp
+++ src/gui/render/render.cpp
@@ -359,8 +359,8 @@ static void halt_render()
 
 constexpr int MAX_SEND_SIZE = 32 + 1024 + 320*224 + 320*224/64; // must be larger than 64kb window size for lz4!
 
-uint8_t input_buf[3*MAX_SEND_SIZE];
-int input_pos;
+uint8_t input_buf[3*MAX_SEND_SIZE], input_buf_lh[3*MAX_SEND_SIZE];
+int input_pos, input_pos_lh;
 uint8_t real_sendbuf[140000];
 uint8_t prevScreen[320*224];
 
@@ -532,7 +532,11 @@ void RENDER_EndUpdate([[maybe_unused]] bool abort)
             }
             if (input_pos >= 2*MAX_SEND_SIZE)
                 input_pos = 0;
+            if (input_pos_lh >= 2*MAX_SEND_SIZE)
+                input_pos_lh = 0;
             uint8_t *sendbuf = input_buf + input_pos;
+            uint8_t *lhbuf = input_buf_lh + input_pos_lh;
+            memset(lhbuf, 'A', 40);
             memcpy(sendbuf, &sendflags, 4);
             if (sendflags & 1) {
                 memcpy(sendbuf+32, &render.pal.rgb, 1024);
@@ -549,6 +553,8 @@ void RENDER_EndUpdate([[maybe_unused]] bool abort)
             origlen = encoded_end - sendbuf;
             input_pos += origlen;
             input_pos = (input_pos + 15) & ~15;
+            input_pos_lh += 40; // I'll send 40 'A's for now
+            input_pos_lh = (input_pos_lh + 15) & ~15;
 
             timespec ts;
             clock_gettime(CLOCK_REALTIME, &ts);
@@ -560,7 +566,9 @@ void RENDER_EndUpdate([[maybe_unused]] bool abort)
             if (sendlen_uh == 0)
                 exit(1);
             memcpy(real_sendbuf, &sendlen_uh, 2);
-            uint16_t sendlen_lh = 0;
+            uint16_t sendlen_lh = LZ4_compress_HC_continue(&stream_lh, (const char *) lhbuf, (char*) real_sendbuf+sendlen_uh+4, 40, sizeof(real_sendbuf)-sendlen_uh-4);
+            if (sendlen_lh == 0)
+                exit(1);
             memcpy(real_sendbuf+2, &sendlen_lh, 2);
             int ret = complete_write(sock, (const char*) real_sendbuf, (unsigned) sendlen_uh+sendlen_lh+4);
             if (ret < 0)
