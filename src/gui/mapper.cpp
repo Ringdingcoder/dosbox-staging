3bb2e6611485fa816a870990044db62707facfbd
Stefan Ring <stefanrin@gmail.com>
2026-02-01 15:42:38 +0100

    joysticks working!
    J1: E-R A
    J2: ;-' ,

--- src/gui/mapper.cpp
+++ src/gui/mapper.cpp
@@ -476,11 +476,11 @@ static struct {
 	bool button_pressed[MAX_VJOY_BUTTONS] = {false};
 } virtual_joysticks[2];
 
-void MAPPER_SetJ(int16_t v)
-{
-    printf("setting J %d\n", (int) v);
-    virtual_joysticks[0].axis_pos[0] = v;
-}
+static struct {
+    bool left_pressed;
+    bool right_pressed;
+    bool button_pressed;
+} fake_joy[2];
 
 
 class CJAxisBind;
@@ -2652,6 +2652,50 @@ void MAPPER_CheckEvent(SDL_Event *event)
 	default: break;
 	}
 
+        if (event->type == SDL_KEYDOWN || event->type == SDL_KEYUP) {
+            bool update = false;
+            switch (event->key.keysym.scancode) {
+            case SDL_SCANCODE_E:
+                fake_joy[0].left_pressed = event->type == SDL_KEYDOWN;
+                update = true;
+                break;
+            case SDL_SCANCODE_R:
+                fake_joy[0].right_pressed = event->type == SDL_KEYDOWN;
+                update = true;
+                break;
+            case SDL_SCANCODE_A:
+                fake_joy[0].button_pressed = event->type == SDL_KEYDOWN;
+                update = true;
+                break;
+            case SDL_SCANCODE_SEMICOLON:
+                fake_joy[1].left_pressed = event->type == SDL_KEYDOWN;
+                update = true;
+                break;
+            case SDL_SCANCODE_APOSTROPHE:
+                fake_joy[1].right_pressed = event->type == SDL_KEYDOWN;
+                update = true;
+                break;
+            case SDL_SCANCODE_COMMA:
+                fake_joy[1].button_pressed = event->type == SDL_KEYDOWN;
+                update = true;
+                break;
+            default:
+                break;
+            }
+
+            if (update) {
+                virtual_joysticks[0].axis_pos[0] =
+                    fake_joy[0].left_pressed == fake_joy[0].right_pressed ?
+                    16384 : (fake_joy[0].left_pressed ? 0 : 32767);
+                virtual_joysticks[0].button_pressed[0] = fake_joy[0].button_pressed;
+                virtual_joysticks[1].axis_pos[0] =
+                    fake_joy[1].left_pressed == fake_joy[1].right_pressed ?
+                    16384 : (fake_joy[1].left_pressed ? 0 : 32767);
+                virtual_joysticks[1].button_pressed[0] = fake_joy[1].button_pressed;
+                printf("set j %d %d\n", virtual_joysticks[1].axis_pos[0], virtual_joysticks[1].button_pressed[0]);
+            }
+        }
+
 	for (auto& group : bindgroups) {
 		if (group->CheckEvent(event)) {
 			return;
@@ -2831,7 +2875,7 @@ static void QueryJoysticks()
 	const bool wants_auto_config = joytype & (JOY_AUTO | JOY_ONLY_FOR_MAPPING);
 
 	// Record how many joysticks are present and set our desired minimum axis
-	const auto num_joysticks = SDL_NumJoysticks();
+	const auto num_joysticks = 2;
 	if (num_joysticks < 0) {
 		LOG_WARNING("MAPPER: SDL_NumJoysticks() failed: %s", SDL_GetError());
 		LOG_WARNING("MAPPER: Skipping further joystick checks");
