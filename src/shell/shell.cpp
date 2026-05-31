c258533006a440672f711427a3e4e12eb42bbb7d
Stefan Ring <stefanrin@gmail.com>
2026-05-31 19:31:48 +0200

    ok, it works this way (order reversed)

--- src/shell/shell.cpp
+++ src/shell/shell.cpp
@@ -1635,8 +1635,8 @@ void SHELL_AlternativeRun()
     image_info.video_mode.is_double_scanned_mode = true;
     image_info.video_mode.pixel_aspect_ratio = Fraction(1, 1);
     vga.draw.lines_scaled = 1;
-    RENDER_SetSize(image_info, 60.);
     VGA_SetupDrawing(0);
+    RENDER_SetSize(image_info, 60.);
 
     shell_networkinit = false;
 
